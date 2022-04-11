4e500489148a4491b806f8620f5142f2a253039e
Sienna Brown <31221385+siennana@users.noreply.github.com>
2022-04-10 17:39:57 -0700

    updated home page

--- src/App.js
+++ src/App.js
@@ -3,22 +3,34 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class="title-wrapper screen">
+		<div class="main-content">
+			<div class="content-wrapper">
+				<div class = "name-title title" >
+					<div class="first">SIENNA </div>
+					<div class="last">BROWN</div>
+					<div class="icon"><i class="fas fa-grin-tongue-squint"></i></div>
+				</div>
+				<div class="typewriter"><h1>Blah</h1></div>
+				<div class="button-wrapper">
+					<a href="pages/users.html">
+						<div class="button">CONTINUE</div>
+					</a>
+				</div>
+			</div>
+		</div>
+		<div id="music" class="panel panel-music">
+			<div class="top_bar">
+				<div class="min bar_click"><i class="far fa-window-minimize"></i></div>
+				<div class="max bar_click"><i class="far fa-window-maximize"></i></div>
+				<div onclick="closeWindow()" id="close" class="exit bar_click"><i class="far fa-window-close"></i></div>
+			</div>
+			<div class="title"></div>
+			<div class="content">
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, repellendus modi qui sapiente omnis voluptas a molestiae! Optio beatae, reprehenderit hic placeat modi, nihil eius earum, ipsum tempore aperiam distinctio.</p>
+			</div>
+		</div>
+	</div>
   );
 }
 
