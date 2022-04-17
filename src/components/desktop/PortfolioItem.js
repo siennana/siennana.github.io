d6a8f06b3c655ee4d7e752d81be3fbad009f5538
Sienna Brown <31221385+siennana@users.noreply.github.com>
2022-04-17 02:52:42 -0500

    reinstalled node modules, updated portfolio

--- src/components/desktop/PortfolioItem.js
+++ src/components/desktop/PortfolioItem.js
@@ -3,9 +3,11 @@ import '../../pages/PortfolioItem.css';
 function PortfolioItem(props) {
 	return (
 		<div>
-			<h2 className='portfolioItem-header'>{props.title}</h2>
-			<p className='portfolioItem-date'>{props.dateRange}</p>
-			<p className='portfolioItem-description'>{props.description}</p>
+      <div className='portfolioItem-wrapper'>
+        <h2 className='portfolioItem-header'>{props.title}</h2>
+        <p className='portfolioItem-date'>{props.dateRange}</p>
+        <p className='portfolioItem-description'>{props.description}</p>
+      </div>
 		</div>
 	);
 }
